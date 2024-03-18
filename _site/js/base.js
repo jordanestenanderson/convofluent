0d5a7f74730bd7cd113b9f6f8c105d39228ee406
Jordan Anderson <jordan@esten.co>
2024-03-18 14:48:50 -0700

    Updated content and design

--- _site/js/base.js
+++ _site/js/base.js
@@ -1,37 +1,31 @@
-AOS.init({
-  	duration: 1000,
-    easing: 'ease-out-quad',
-    once: true,
-})
-
-$(document).ready(function(){
-  $('.step-graphic').each(function(){
-   var triggerValue = $(this).attr('trigger');
-   $(this).click(function(){
-      $("body, .lightbox, .overlay, .lightbox-content").addClass("open");
-      $("." + triggerValue).addClass("open");
-   });
-  });
-});
-
-$(document).ready(function(){
-  $('.toggle-button').each(function(){
-   var industryValue = $(this).attr('trigger');
-   $(this).click(function(){
-      $(".toggle-button, .industry-image, .industry-list").removeClass("active");
-      $("." + industryValue).addClass("active");
-   });
-  });
-});
- 
-$(".video-overlay").click(function(){
-   $("body, .lightbox, .overlay, .lightbox-content, .lightbox-step").addClass("open");
-   $(".lightbox-step iframe").attr("src", "https://player.vimeo.com/video/863375235?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&autoplay=1");
-});
-
-$(".lightbox-close, .lightbox-scroller").click(function(){
-   $("body, .lightbox, .overlay, .lightbox-content, .lightbox-step").removeClass("open");
-   $(".lightbox-step iframe").attr("src", "https://player.vimeo.com/video/863375235?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479");
-});
+(function update_time(){
+  var now = moment().format('h:mm:ss a', 'Z');
+  $('#curTime').text(now);
+  setTimeout(update_time, 1000);
+})();
+(function current_date(){
+  var curDate = moment().format('MMMM Do YYYY');
+  $('#curDate').text(curDate);
+})();
+(function show_hours(){
+  var curHour = moment().format('h');
+  $('#curHour').text(curHour);
+})();
+(function tom_date(){
+  var today = moment();
+  var tomorrow = today.add(1, 'days');
+  var tomDate = moment(tomorrow).format("MMMM Do YYYY");
+  $('#tomDate').text(tomDate);
+})();
+(function tomorrow_time(){
+  var x = new Date();
+  var currentTimeZoneOffsetInHours = x.getTimezoneOffset();
+  $('#tomTime').text(currentTimeZoneOffsetInHours);
+})();
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var body = document.getElementById("body-container");
+  body.classList.add("loaded");
+}, false);
